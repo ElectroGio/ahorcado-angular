ba93120e7a9e8aa142226400f6e03bd5a4abb8e1
Alejandro Alonso Taratiel <alejalonta@gmail.com>
2018-06-05 13:51:48 +0200

    Mostrar las letras correctas en la palabra oculta

--- ahorcado-angular/src/app/app.component.ts
+++ ahorcado-angular/src/app/app.component.ts
@@ -6,9 +6,25 @@ import { cleanSession } from 'selenium-webdriver/safari';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
+
+  palabra = 'AGUACATE';
+  palabraOculta = '';
+
   letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
   constructor() {
-    console.log('Se acaba de crear el App Component');
+    this.palabraOculta = '_ '.repeat( this.palabra.length );
+  }
+
+  comprobar( letra ) {
+    const palabraOcultaArr = this.palabraOculta.split(' ');
+
+    for (let i = 0; i < this.palabra.length; i++) {
+      if ( this.palabra[i] === letra ) {
+        palabraOcultaArr[i] = letra;
+      }
+
+      this.palabraOculta = palabraOcultaArr.join(' ');
+    }
   }
 }
